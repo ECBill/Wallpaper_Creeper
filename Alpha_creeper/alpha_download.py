0a63be124091878260ea5ad54ff23a267de680ea
BillCheng <bill.cheng@networkprovide.com>
2023-07-10 09:38:11 +0800

    the last day in Weiyu

--- Alpha_creeper/alpha_download.py
+++ Alpha_creeper/alpha_download.py
@@ -1,3 +1,5 @@
+import time
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
@@ -9,11 +11,15 @@ def download(url):
     opt = uc.ChromeOptions()
     #opt.add_argument(argument="headless")
     browser = uc.Chrome(options=opt)
-    i = 1
+    i = 0
     srcs = list()
     while True:
+        i+=1
         browser.get(url + str(i))
-        html = browser.page_source
+        try:
+            html = browser.page_source
+        except:
+            break
         soup = BeautifulSoup(html, 'lxml')
         link_1 = soup.find_all('img', class_='img-responsive big-thumb thumb-desktop')
         link_2 = soup.find_all('img', class_='img-responsive thumb-desktop')
@@ -21,16 +27,24 @@ def download(url):
         links = link_1+link_2+link_3
         #print(links)
         if links == []:
-            break
+            time.sleep(10)
+            html = browser.page_source
+            soup = BeautifulSoup(html, 'lxml')
+            link_1 = soup.find_all('img', class_='img-responsive big-thumb thumb-desktop')
+            link_2 = soup.find_all('img', class_='img-responsive thumb-desktop')
+            link_3 = soup.find_all('img', class_='img-responsive big-thumb')
+            links = link_1 + link_2 + link_3
+            if links == []:
+                break
         for img_tag in links:
             src = img_tag.get('src')
             srcs.append(src)
-        i += 1
     l=len(srcs)
     print(l)
     path = './result/'
     j=1
     for src in srcs:
+        src = src.replace("thumbbig-","")
         response = requests.get(src)
         with open(os.path.join(path, os.path.basename(src)), 'wb') as f:
             f.write(response.content)
--- Alpha_creeper/alphacoders_creeper.py
+++ Alpha_creeper/alphacoders_creeper.py
@@ -4,6 +4,6 @@ import alpha_download
 # ID='718'
 # url = 'https://wall.alphacoders.com/by_collection.php?id='+ID+ '&quickload=230+&page='
 #url = alpha_url.get_url_byapi('splatoon')
-url = alpha_url.get_url('Naruto')
-print(url)
-#alpha_download.download(url)
+url = alpha_url.get_url('Re:ZERO -Starting Life in Another World')
+#print(url)
+alpha_download.download(url)
